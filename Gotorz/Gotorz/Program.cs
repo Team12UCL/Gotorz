4bc599f2fec8bbcb9991997b15d70092688e2359
ikvi63270 <159004266+ikvi63270@users.noreply.github.com>
2025-05-21 09:20:54 +0200

    Update Program.cs
    RedirectToLogin fix

--- Gotorz/Gotorz/Program.cs
+++ Gotorz/Gotorz/Program.cs
@@ -83,19 +83,19 @@ public class Program
         builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
         builder.Logging.AddConsole();
 
-        builder.Services.ConfigureApplicationCookie(options =>
-        {
-            options.Events.OnRedirectToLogin = context =>
-            {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                return Task.CompletedTask;
-            };
-            options.Events.OnRedirectToAccessDenied = context =>
-            {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                return Task.CompletedTask;
-            };
-        });
+        //builder.Services.ConfigureApplicationCookie(options =>
+        //{
+        //    options.Events.OnRedirectToLogin = context =>
+        //    {
+        //        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        //        return Task.CompletedTask;
+        //    };
+        //    options.Events.OnRedirectToAccessDenied = context =>
+        //    {
+        //        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        //        return Task.CompletedTask;
+        //    };
+        //});
 
         var app = builder.Build();
 
